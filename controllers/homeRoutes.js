153581d11626513bd86716ba244e703c3c0f080f
Wes Howard <BigRonald42@hotmail.com>
2021-04-08 10:47:37 -0700

    added comment routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,14 +49,14 @@ router.get('/review/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+cd
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Review }],
     });
 
     const user = userData.get({ plain: true });
