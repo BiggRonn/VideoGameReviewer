bd4547b3db779df9cdc2d217441a80999d461712
Wes Howard <BigRonald42@hotmail.com>
2021-04-08 12:26:51 -0700

    rawgRoutes

---
+++ controllers/api/rawgRoutes.js
@@ -0,0 +1,48 @@
+
+
+app.get("/api/search", async function (req, res) {
+
+    try{
+    const slug = document.querySelector('#game-title').toLowerCase().replaceText(' ', '-');
+
+    gameInfo = [];
+    apiKey = process.env.API_KEY
+    const url = `https://api.rawg.io/api/games/${slug}?key${apiKey}`
+    
+    var rawgUrl = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+    })
+
+    res.setHeader("Content-Type", "application/json");
+    data.data.pipe(res);
+    }catch (err){
+        res.json(err);
+    }
+
+      
+    
+      
+}
+
+app.get("/api/search", async function (req, res) {
+    try {
+      const query = req.url.split("?")[1];
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&${query}`;
+  ​
+      const data = await axios({
+        method: "get",
+        url: url,
+        responseType: "stream",
+      });
+  ​
+      res.setHeader("Content-Type", "application/json");
+      data.data.pipe(res);
+      
+    } catch (err) {
+      res.json(err);
+    }
+  });
+  ​
+  
