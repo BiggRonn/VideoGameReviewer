2a2ab7ee1252fd92a2ddafd81bd1edf23665c19d
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-11 14:00:17 -0700

    fixed display issues

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,16 @@
 //Importing dependencies, models, and authentication.
-const router = require('express').Router();
-const { Review, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Review, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const reviewData = await Review.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -19,31 +19,31 @@ router.get('/', async (req, res) => {
     const reviews = reviewData.map((review) => review.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      reviews, 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      reviews,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/review/:id', async (req, res) => {
+router.get("/review/:id", async (req, res) => {
   try {
     const reviewData = await Review.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
-    const review = reviewData.get(({ plain: true }));
+    const review = reviewData.get({ plain: true });
 
-    res.render('edit-review', {
+    res.render("edit-review", {
       ...review,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -51,33 +51,33 @@ router.get('/review/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Review }],
     });
 
-    const user = userData.get(({ plain: true }));
+    const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
