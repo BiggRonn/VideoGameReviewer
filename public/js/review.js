35d2ee0e42180c8e831bc4b8fcaa574436e03cec
Wes Howard <BigRonald42@hotmail.com>
2021-04-11 16:03:41 -0700

    got new reviews to post

--- public/js/review.js
+++ public/js/review.js
@@ -1,15 +1,11 @@
 const formHandler = async (event) => {
   event.preventDefault();
 
-  // const platform = document.querySelector("#platform").value;
-  // const game_genre = document.querySelector("#game_genre").value;
-  // const description = document.querySelector("#description").value;
-  // const game_title = document.querySelector("#game_title").value;
+  const platform = document.getElementById("platform").innerHTML;
+  const game_genre = document.getElementById("game_genre").innerHTML;
+  const description = document.getElementById("description").innerHTML;
+  const game_title = document.getElementById("game_title").innerHTML;
 
-  const platform = "PSOne";
-  const game_genre = "Fun RPG FPS Puzzle";
-  const description = "The description of this game is everything we hoped for";
-  const game_title = "Best GAME created EVER";
 
   if (game_title) {
     const reviewData = await fetch("/api/reviews", {
