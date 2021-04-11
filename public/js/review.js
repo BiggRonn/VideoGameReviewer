5dd2f77feda3506731eb2225866ebbc0601ba772
Wes Howard <BigRonald42@hotmail.com>
2021-04-11 15:40:05 -0700

    added notes/clue to help us post new review

--- public/js/review.js
+++ public/js/review.js
@@ -1,15 +1,20 @@
 const formHandler = async (event) => {
   event.preventDefault();
 
-  const comment = document.querySelector("#review-desc").value;
-  const game_genre = document.querySelector("#game_genre").value;
-  const description = document.querySelector("#description").value;
-  const game_title = document.querySelector("#game_title").value;
+  // const platform = document.querySelector("#platform").value;
+  // const game_genre = document.querySelector("#game_genre").value;
+  // const description = document.querySelector("#description").value;
+  // const game_title = document.querySelector("#game_title").value;
 
-  if (comment) {
+  const platform = "PSOne";
+  const game_genre = "Fun RPG FPS Puzzle";
+  const description = "The description of this game is everything we hoped for";
+  const game_title = "Best GAME created EVER";
+
+  if (game_title) {
     const reviewData = await fetch("/api/reviews", {
       method: "POST",
-      body: JSON.stringify({game_title, game_genre, description, comment }),
+      body: JSON.stringify({game_title, game_genre, description, platform}),
       headers: {
         "Content-Type": "application/json",
       },
